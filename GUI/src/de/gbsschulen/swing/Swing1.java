8f3f44b22846bf4a46cdd1fcce2a9cc8c5874ca9
Marco Kiefer <kiefer.marco@me.com>
2018-12-05 15:02:28 +0100

    Einführung Swing Fenster

--- GUI/src/de/gbsschulen/swing/Swing1.java
+++ GUI/src/de/gbsschulen/swing/Swing1.java
@@ -1,5 +1,26 @@
 package de.gbsschulen.swing;
 
-public class Swing1 {
+import java.awt.Color;
 
+import javax.swing.*;
+
+public class Swing1 extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Swing1() {
+		this.setSize(500, 400);
+		this.setTitle("Mein erstes Swing Fenster");
+		this.setBackground(Color.blue);
+	}
+
+	public static void main(String[] args) {
+		Swing1 sw1 = new Swing1();
+		sw1.setVisible(true);
+		sw1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
 }

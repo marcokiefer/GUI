98520e6be035f0598284d098c038c36addf4cb09
Marco Kiefer <kiefer.marco@me.com>
2018-12-05 15:11:25 +0100

    Swing 2 Fenster erstellt

---
+++ GUI/src/de/gbsschulen/swing/Swing2.java
@@ -0,0 +1,29 @@
+package de.gbsschulen.swing;
+
+import java.awt.Container;
+import javax.swing.*;
+
+public class Swing2 extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	Container c;
+	
+	JLabel lb;			// Anzeige Box
+	
+	
+	public Swing2 () {
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
